c91ff0b53180de16ae93a97e674e11d42b6121a3
Josh Orfanakis <jorfanakis@planetdata.com>
2017-09-28 13:44:03 -0600

    Added Greet new user and notified existing users of new user.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit from Admin text welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.broadcast.emit from Admin text new user joined
+
   socket.on('createMessage', (data) => {
     console.log('Create Message');
     io.emit('newMessage', {
@@ -23,6 +38,13 @@ io.on('connection', (socket) => {
       text: data.text,
       createdAt: new Date().getTime()
     });
+/*
+    socket.broadcast.emit('newMessage', {
+      from: data.from,
+      text: data.text,
+      createdAt: new Date().getTime()
+    });
+*/
   });
 
   socket.on('disconnect', () => {
