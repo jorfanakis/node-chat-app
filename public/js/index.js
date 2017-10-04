0be6f3d93a8040bf46cbc41b57f8b6f83e1b77fa
Josh Orfanakis <jorfanakis@planetdata.com>
2017-10-04 13:56:01 -0600

    Added auto-scroll + Mustache template fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
   const socket = io();
+
+  function scrollToBottom() {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+  }
+
   socket.on('connect', function() {
     console.log('Connected to server');
   });
@@ -13,10 +31,11 @@
     const html = Mustache.render(template, {
       text: data.text,
       from: data.from,
-      createAt: formattedTime
+      createdAt: formattedTime
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
   });
 
   socket.on('newLocationMessage', function(data) {
@@ -27,6 +46,7 @@
       url: data.url
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
   });
 
   jQuery('#message-form').on('submit', function(e) {
